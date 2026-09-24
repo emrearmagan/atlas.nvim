69ed214b10585820caddefb30557f18174a6f6bb
Emre Armagan <emrearmagan.dev@gmail.com>
2026-09-24 02:15:08 +0200

    fix: remove commits page

--- lua/atlas/ui/repository/pages/init.lua
+++ lua/atlas/ui/repository/pages/init.lua
@@ -3,7 +3,6 @@ local issues = require("atlas.ui.repository.pages.issues")
 local builds = require("atlas.ui.repository.pages.builds")
 local branches = require("atlas.ui.repository.pages.branches")
 local tags = require("atlas.ui.repository.pages.tags")
-local commits = require("atlas.ui.repository.pages.commits")
 
 ---@class RepositoryPage
 ---@field key string
@@ -17,7 +16,7 @@ local M = {}
 ---@param provider PullsProvider|IssuesProvider
 ---@return RepositoryPage[]
 function M.get(provider)
-	local pages = { overview, issues, builds, branches, tags, commits }
+	local pages = { overview, issues, builds, branches, tags }
 	local ui = provider.capabilities.ui and provider.capabilities.ui.repository
 	if ui then
 		vim.list_extend(pages, ui.pages())
